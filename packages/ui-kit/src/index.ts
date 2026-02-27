88d5eff6152c6e59f6eb723b84de4a053791acf7
Hasan Sarwer <hasanmathju@gmail.com>
2026-02-27 12:28:56 +0600

    export all components

---
+++ packages/ui-kit/src/index.ts
@@ -0,0 +1,7 @@
+export { default as AppText } from "./components/AppText";
+export { default as Button } from "./components/Button";
+export { default as Card } from "./components/Card";
+export { default as Input } from "./components/Input";
+export { default as Screen } from "./components/Screen";
+
+export * from "./theme";
