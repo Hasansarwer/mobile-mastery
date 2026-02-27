14ed307ebeb5c5b9145cbdb68f764ae5cc12b5c1
Hasan Sarwer <hasanmathju@gmail.com>
2026-02-28 01:32:26 +0600

    feat(ui-kit): add heading, spacer, row, and column

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -3,5 +3,9 @@ export { default as Button } from "./components/Button";
 export { default as Card } from "./components/Card";
 export { default as Input } from "./components/Input";
 export { default as Screen } from "./components/Screen";
+export { default as Heading } from "./components/Heading";
+export { default as Spacer } from "./components/Spacer";
+export { default as Row } from "./components/Row";
+export { default as Column } from "./components/Column";
 
 export * from "./theme";
